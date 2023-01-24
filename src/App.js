a7f4f27bedf5e86b72d12840c9e62ed6d508d1ff
Jonathan Ndinga <jonathanmpaka.ndinga@gbs-it.de>
2023-01-24 08:58:46 +0100

    added title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <div className="App">
+      Homepage
+      <div>
+        <h1>Title</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
