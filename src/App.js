995e4111bc038efea3b404955d05de59a3abda85
Jonathan Ndinga <jonathanmpaka.ndinga@gbs-it.de>
2023-01-24 09:11:52 +0100

    deleted boiler plate react code and customized the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Homepage
-      <div>
-        <h1>Title</h1>
-      </div>
-    </div>
-  );
+  return <div className="App">Homepage</div>;
 }
 
 export default App;
